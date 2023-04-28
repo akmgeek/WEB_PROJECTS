426dd8d136cf21fb4d393b99e1ba2e83df77179d
Amit Maurya <106646158+akmgeek@users.noreply.github.com>
2023-04-28 12:28:55 +0530

    Add files via upload

--- WeatherAPP/script.js
+++ WeatherAPP/script.js
@@ -10,13 +10,33 @@ const weatherButton = document.querySelector('#weatherButton');
 weatherButton.addEventListener('click', () => {
   console.log("rererererere")
   const apiKey = '8268a916c3cb4c81b3d185255232704';
-  const city = 'London';
+  const city = 'Noida';
   const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
 
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
       console.log(data);
+        const current_temp_c = data.current.temp_c;
+        const current_condition = data.current.condition;
+        const current_humidity = data.current.temp_c;
+        const current_temp_f = data.current.temp_c;
+
+        const location_country = data.location.country;
+        const locattion_region = data.location.region;
+        const location_name = data.location.name;
+        // const location = data.location.name
+        // const location = data.location.name
+        // const location = data.location.name
+        // const location = data.location.name
+        // console.log(current)
+        // console.log(current_condition.text)
+
+        document.getElementById("city-name").innerHTML = location_name;
+        document.getElementById("country-name").innerHTML = location_country;
+        document.getElementById("current-temp").innerHTML = current_temp_c;
+        document.getElementById("condition-text").innerHTML = current_condition.text;
+ 
       // Do something with the weather data
     })
     .catch(error => console.error(error));
