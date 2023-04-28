b5f960b276f9ca4e13ed6da0c92bbf28667c99df
Amit Maurya <106646158+akmgeek@users.noreply.github.com>
2023-04-28 12:42:52 +0530

    Add files via upload

--- WeatherAPP/script.js
+++ WeatherAPP/script.js
@@ -1,45 +1,35 @@
-// const apiKey = '8268a916c3cb4c81b3d185255232704'; // replace with your own API key
-// const city = 'New York'; // replace with the name of the city you want to get the weather for
-// const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
-// const getWeatherButton = document.querySelector('#getWeatherButton');
-// const getWeatherButton = document.getElementById("#getWeatherButton")
 document.addEventListener('DOMContentLoaded', () => {
 
-const weatherButton = document.querySelector('#weatherButton');
-
-weatherButton.addEventListener('click', () => {
-  console.log("rererererere")
   const apiKey = '8268a916c3cb4c81b3d185255232704';
-  const city = 'Noida';
-  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
 
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
+  function getWeatherData(latitude, longitude) {
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}`;
+
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
         const current_temp_c = data.current.temp_c;
-        const current_condition = data.current.condition;
-        const current_humidity = data.current.temp_c;
-        const current_temp_f = data.current.temp_c;
+        const current_condition = data.current.condition.text;
+        const current_humidity = data.current.humidity;
+        const current_temp_f = data.current.temp_f;
 
         const location_country = data.location.country;
-        const locattion_region = data.location.region;
+        const location_region = data.location.region;
         const location_name = data.location.name;
-        // const location = data.location.name
-        // const location = data.location.name
-        // const location = data.location.name
-        // const location = data.location.name
-        // console.log(current)
-        // console.log(current_condition.text)
 
         document.getElementById("city-name").innerHTML = location_name;
         document.getElementById("country-name").innerHTML = location_country;
-        document.getElementById("current-temp").innerHTML = current_temp_c;
-        document.getElementById("condition-text").innerHTML = current_condition.text;
- 
-      // Do something with the weather data
-    })
-    .catch(error => console.error(error));
-});
+        document.getElementById("current-temp").innerHTML = `${current_temp_c}°C (${current_temp_f}°F)`;
+        document.getElementById("condition-text").innerHTML = current_condition;
+      })
+      .catch(error => console.error(error));
+  }
+
+  navigator.geolocation.getCurrentPosition(position => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    getWeatherData(latitude, longitude);
+  });
 
 });
